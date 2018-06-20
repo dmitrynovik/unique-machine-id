b5566c8d39e8f80eb7a1e96b9a4aaf7c4e07b1ff
Dmitry Novik <dmitry.novik@gmail.com>
2018-06-20 19:23:39 +1000

    Init

---
+++ MachineUniqueId/Machine.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
+
+namespace MachineUniqueId
+{
+    public class Machine
+    {
+        public string GetCpuId() => GetWmiProp("win32_processor", "processorID");
+        public string GetHddId() => GetWmiProp("Win32_DiskDrive", "SerialNumber");
+
+        public string GetMacAddress() => NetworkInterface
+            .GetAllNetworkInterfaces()
+            .Where(nic => nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            .Select(nic => nic.GetPhysicalAddress().ToString())
+            .FirstOrDefault() ?? string.Empty;
+
+        private static string GetWmiProp(string className, string property)
+        {
+            var managementObjects = new ManagementClass(className).GetInstances();
+            foreach (var mo in managementObjects)
+            {
+                //Get only the first CPU's ID
+                var cpu = mo.Properties[property].Value.ToString();
+                if (!string.IsNullOrWhiteSpace(cpu))
+                    return cpu;
+            }
+            return string.Empty;
+        }
+
+        public override int GetHashCode() => (GetCpuId().GetHashCode() * 13) ^ (GetHddId().GetHashCode() * 17) ^ (GetMacAddress().GetHashCode() * 23);
+
+        public override string ToString() => $"CPU: {GetCpuId()}, HDD: {GetHddId()}, MAC: {GetMacAddress()}";
+
+        public override bool Equals(object obj) => obj is Machine other && other.GetMacAddress() == GetMacAddress() &&
+                                                   other.GetCpuId() == GetCpuId() && other.GetHddId() == GetHddId();
+    }
+}
---
+++ MachineUniqueId/MachineTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MachineUniqueId
+{
+    [TestFixture]
+    public class MachineTest
+    {
+        [Test]
+        public void GetCpu()
+        {
+            var cpu = new Machine().GetCpuId();
+            Console.WriteLine($"CPU: {cpu}");
+            Assert.IsNotEmpty(cpu);
+        }
+
+        [Test]
+        public void GetHdd()
+        {
+            var hdd = new Machine().GetHddId();
+            Console.WriteLine($"CPU: {hdd}");
+            Assert.IsNotEmpty(hdd);
+        }
+
+        [Test]
+        public void GetMac()
+        {
+            var mac = new Machine().GetMacAddress();
+            Console.WriteLine($"MAC: {mac}");
+            Assert.IsNotEmpty(mac);
+        }
+
+        [Test]
+        public void GetHardDriveUniqueId()
+        {
+            var hdd = new Machine().GetHddId();
+            Console.WriteLine($"HDD: {hdd}");
+            Assert.IsNotEmpty(hdd);
+        }
+    }
+}
